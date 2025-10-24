43cbbd18ddbde81aba501b93e96b59e4b5baf6bb
ZonG0D <151726005+ZonG0D@users.noreply.github.com>
2025-10-24 14:04:36 -0400

    Restore NAT masquerade in nftables configuration

--- build-rootfs.sh
+++ build-rootfs.sh
@@ -233,6 +233,7 @@ EOF
 cat > "${ROOTFS_DIR}/etc/nftables.conf" <<'EOF'
 #!/usr/sbin/nft -f
 flush ruleset
+
 table inet filter {
   chain input {
     type filter hook input priority 0;
@@ -252,6 +253,17 @@ table inet filter {
   chain output {
     type filter hook output priority 0;
     accept
+  }
+}
+
+table ip nat {
+  chain prerouting {
+    type nat hook prerouting priority -100;
+  }
+
+  chain postrouting {
+    type nat hook postrouting priority 100;
+    oifname "wan" masquerade
   }
 }
 EOF
